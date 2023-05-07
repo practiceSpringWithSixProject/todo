210d9a60b45c7fb3d8402fe7abfe1df18a6ffe3d
Lee seung chan <dltmdcks702@gmail.com>
2023-05-07 09:44:35 +0900

    feat: config auto migration on local db
    1. postgreSQL에 JPA에서 automigration해주도록 설정
    1. docker 설정이 무의미할 정도로 잘 안된다는 것을 느끼는 중

--- src/main/java/com/example/todo/entities/Author.java
+++ src/main/java/com/example/todo/entities/Author.java
@@ -1,4 +1,59 @@
 package com.example.todo.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+
+@Entity
 public class Author {
+    private @Id @GeneratedValue Long id;
+    private String name;
+
+    public Author(String bilboBaggins) {
+        this.name = bilboBaggins;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (!(o instanceof Author)) {
+            return false;
+        }
+
+        Author author = (Author) o;
+
+        return Objects.equals(this.id, author.id) && Objects.equals(this.name, author.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id, this.name);
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" + "id=" + this.id + ", name='" + this.name + '\'' + '}';
+    }
 }
