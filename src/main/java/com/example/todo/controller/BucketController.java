e7b7a3eac605ee174553619c62dbcb72b382554c
SoonWookHwang <bravadosw@naver.com>
2023-05-12 01:33:15 +0900

    feat: add feature getting Bucket-list found by Author_Id

--- src/main/java/com/example/todo/controller/BucketController.java
+++ src/main/java/com/example/todo/controller/BucketController.java
@@ -3,7 +3,9 @@ package com.example.todo.controller;
 import com.example.todo.dto.BucketDto;
 import com.example.todo.model.Bucket;
 import com.example.todo.service.BucketService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +20,11 @@ public class BucketController {
   @PostMapping("/bucket/{authorId}")
   public Bucket createBucket(@RequestBody BucketDto bucketDto, @PathVariable Long authorId) {
     return bucketService.createBucket(bucketDto, authorId);
+  }
+
+  @GetMapping("/bucket/{authorId}")
+  public List<Bucket> getBucketsByAuthorId(@PathVariable Long authorId) {
+    return bucketService.getAllBucketsByAuthorId(authorId);
   }
 
 
--- src/main/java/com/example/todo/repository/BucketRepository.java
+++ src/main/java/com/example/todo/repository/BucketRepository.java
@@ -1,8 +1,14 @@
 package com.example.todo.repository;
 
 import com.example.todo.model.Bucket;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BucketRepository extends JpaRepository<Bucket, Long> {
+
+  @Query("SELECT b from Bucket b where b.author.id = :authorId")
+  Optional<List<Bucket>> findAllByAuthor_Id(Long authorId);
 
 }
