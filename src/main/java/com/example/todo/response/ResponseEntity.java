bd24649d17494ff8e476e569115b8b39ffa94c32
SoonWookHwang <bravadosw@naver.com>
2023-05-15 01:17:46 +0900

    refactor: change response type Entity to custom ResponseEntity

---
+++ src/main/java/com/example/todo/response/ResponseEntity.java
@@ -0,0 +1,37 @@
+package com.example.todo.response;
+
+import java.net.http.HttpHeaders;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public class ResponseEntity<T> {
+
+  private HttpStatus status;
+  private T data;
+  private Error error;
+
+  public static <T> ResponseEntity<T> success(T data) {
+    return new ResponseEntity<>(HttpStatus.OK, data, null);
+  }
+
+  public static <T> ResponseEntity<T> successHeader(T data, HttpHeaders headers) {
+    return new ResponseEntity<>(HttpStatus.OK, data, null);
+  }
+
+  public static <T> ResponseEntity<T> fail(HttpStatus status, String message) {
+    return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR, null,
+        new Error(status.value(), message));
+  }
+
+  @Getter
+  @AllArgsConstructor
+  static class Error {
+
+    private int statusCode;
+    private String message;
+  }
+
+}
