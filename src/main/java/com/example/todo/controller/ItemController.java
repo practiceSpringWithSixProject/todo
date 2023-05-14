c9f3e6fcb36a264e38f5c57919a9c8cdf11b5660
SoonWookHwang <bravadosw@naver.com>
2023-05-15 02:10:19 +0900

    refactor: change response type Entitiy to custom ResponseEntity

--- src/main/java/com/example/todo/controller/ItemController.java
+++ src/main/java/com/example/todo/controller/ItemController.java
@@ -3,8 +3,10 @@ package com.example.todo.controller;
 import com.example.todo.dto.ToDoItemDto;
 import com.example.todo.model.Bucket;
 import com.example.todo.model.ToDoItem;
+import com.example.todo.response.ResponseEntity;
 import com.example.todo.service.ToDoItemService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,24 +18,45 @@ public class ItemController {
   private final ToDoItemService itemService;
 
   @PostMapping("/items/{authorId}")
-  public ToDoItem createItem(@RequestBody ToDoItemDto requestDto, @PathVariable Long authorId) {
-    return itemService.createItem(requestDto, authorId);
+  public ResponseEntity<ToDoItem> createItem(@RequestBody ToDoItemDto requestDto,
+      @PathVariable Long authorId) {
+    try {
+      ToDoItem newItem = itemService.createItem(requestDto, authorId);
+      return ResponseEntity.success(newItem);
+    } catch (IllegalArgumentException e) {
+      return ResponseEntity.fail(HttpStatus.BAD_REQUEST, e.getMessage());
+    }
   }
 
   @GetMapping("/items")
-  public ToDoItem readOneItem(@RequestParam(value = "itemId") Long itemId) {
-    return itemService.readItem(itemId);
+  public ResponseEntity<ToDoItem> readOneItem(@RequestParam(value = "itemId") Long itemId) {
+    try {
+      ToDoItem foundItem = itemService.readItem(itemId);
+      return ResponseEntity.success(foundItem);
+    } catch (IllegalArgumentException e) {
+      return ResponseEntity.fail(HttpStatus.BAD_REQUEST, e.getMessage());
+    }
   }
 
   @GetMapping("/items/all/{authorId}")
-  public List<ToDoItem> readItemsByAuthorId(@PathVariable Long authorId) {
-    return itemService.readItemsByAuthorId(authorId);
+  public ResponseEntity<List<ToDoItem>> readItemsByAuthorId(@PathVariable Long authorId) {
+    try {
+      List<ToDoItem> foundItems = itemService.readItemsByAuthorId(authorId);
+      return ResponseEntity.success(foundItems);
+    } catch (IllegalArgumentException e) {
+      return ResponseEntity.fail(HttpStatus.BAD_REQUEST, e.getMessage());
+    }
   }
 
   @PutMapping("/items/{itemId}")
-  public ToDoItem updateItem(@PathVariable Long itemId,
+  public ResponseEntity<ToDoItem> updateItem(@PathVariable Long itemId,
       @RequestParam(value = "bucketId", required = false) Long bucketId,
       @RequestBody(required = false) ToDoItemDto dto) {
-    return itemService.updateTodoItem(dto, itemId, bucketId);
+    try {
+      ToDoItem updatedItem = itemService.updateTodoItem(dto, itemId, bucketId);
+      return ResponseEntity.success(updatedItem);
+    } catch (IllegalArgumentException e) {
+      return ResponseEntity.fail(HttpStatus.BAD_REQUEST, e.getMessage());
+    }
   }
 }
